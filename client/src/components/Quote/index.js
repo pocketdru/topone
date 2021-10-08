893c89f2c68f14dab814aaa24fd22f20f9844c58
pocketdru <pocketdru@gmail.com>
2021-10-07 18:10:23 -0600

    pu and del prices

--- client/src/components/Quote/index.js
+++ client/src/components/Quote/index.js
@@ -2,6 +2,8 @@ import { raw } from "body-parser";
 import React, { Component } from "react";
 import API from "../utils/Api";
 import zones from "../../assets/zones/zones.json";
+import prices from "../../assets/zones/price_list.json";
+
 import "./style.css";
 
 class Quote extends Component {
@@ -9,46 +11,19 @@ class Quote extends Component {
     componentDidMount() {
         // const apiURL = ""
     }
-    state = {
-        price: 0,
-        address: [],
-        quote: [],
-        bookSearch: ""
-      };
+
       handleFormSubmit = event => {
         event.preventDefault();
-        var zipCodeToString = String(this.refs.puZip.value);
-        // console.log(zones.zones[0]);
-        // console.log(zipCodeToString.slice(0,3));
-        // var areaCode = zipCodeToString.slice(0,3);
-        // console.log(areaCode);
-        // if (zones.zone_126[0] == areaCode) {
-        //     console.log("It's a zone 126!")
-        // } else {
-        //     console.log("Wrong Area");
-        // }
-console.log(zones.zones[0].areaCodes.lenght);
-    
-        // for (var i=0 ; i < zones.zones.length ; i++)
-        // {
-        //     console.log(zones.zones[0].areaCodes[i].filter(checkArea));
-        // }
-        var keys = Object.keys(zones);
-        console.log(keys.lenght);
-        function checkArea(code) {
-            return code < 804;
-        }
-        for (var i = 0; zones.zones.length > i; i += 1) {
-            // console.log(zones.zones[i].areaCodes);
-
-            for (var k in zones.zones[i].areaCodes ) {
-                
-                console.log(zones.zones[i].areaCodes[k])
-                if (zones.zones[i].areaCodes[k] == "806") {
-                    return console.log(zones.zones[i].zone);
-                }
-            }
-        }
+        var puZipCodeToString = String(this.refs.puZip.value);
+        var delZipCodeToString = String(this.refs.delZip.value);
+        var puAreaCode = puZipCodeToString.slice(0,3);
+        var delAreaCode = delZipCodeToString.slice(0,3);
+        console.log( "Pick up area code is " + puAreaCode + ", delivery area code is " + delAreaCode);
+        var puPrice = "";
+        var delPrice = "";
+        this.areaCodeGet(puAreaCode);
+        puPrice = price;
+        this.areaCodeGet(delAreaCode);
         // const collections = [];
         // var newAddress ={
         //     pickUp: this.refs.puZip.value,
@@ -59,6 +34,32 @@ console.log(zones.zones[0].areaCodes.lenght);
         // this.setState({address: newAddress})
         // this.apiCall();
 
+    }
+    areaCodeGet = (code) => {
+        var code = code;
+        for (var i = 0; zones.zones.length > i; i += 1) {
+            for (var k in zones.zones[i].areaCodes ) {
+                // console.log(zones.zones[i].areaCodes[k])
+                if (zones.zones[i].areaCodes[k] == code) {
+                    code = zones.zones[i].zone;
+                    // console.log(puareaCode);
+                    this.priceGet(code);
+                    return code;
+                }   
+            }
+        }
+    }
+    priceGet = (zone) => {
+        console.log(zone);
+        var zone = zone;
+        for (var i = 0; prices.prices.length > i; i += 1) {
+            if (prices.prices[i].zone == zone) {
+                console.log(prices.prices[i].price);
+                var price = prices.prices[i].price;
+                return price;
+            }
+        }
+        return price;
     }
     // apiCall = () => {
 //         console.log(this.refs.puZip.value);
@@ -82,120 +83,6 @@ console.log(zones.zones[0].areaCodes.lenght);
 //         //  });
 //          var raw = JSON.stringify({
              
-//             "shipment": {
-//                 "carrier_id": "ef865ffe-3b16-4ae2-91d2-95dfb3725a11",
-//                   "pickup_date": "2021-10-10",
-//                   "packages": [
-//                       {
-//                       "code": "PKG",
-//                       "freight_class": 60,
-//                       "nmfc_code": "161630",
-//                       "description": "Paperback books",
-//                       "dimensions": {
-//                           "width": 40,
-//                           "height": 40,
-//                           "length": 48,
-//                           "unit": "inches"
-//                       },
-//                       "weight": {
-//                           "value": 1500,
-//                           "unit": "pounds"
-//                       },
-//                       "quantity": 3,
-//                       "stackable": false,
-//                       "hazardous_materials": false
-//                       }
-//                   ],
-//                   "options": [
-//                       {
-//                       "code": "LFTP"
-//                       },
-//                       {
-//                       "code": "HAZ",
-//                       "attributes": {
-//                           "name": "Contact Person",
-//                           "phone": "7704865900"
-//                       }
-//                       }
-//                   ],
-//                   "ship_from": {
-//                       "account": "710535191",
-//                       "contact": {
-//                           "name": "John Doe",
-//                           "phone_number": "1111111111",
-//                           "email": "johndoe@test.com"
-//                       },
-//                       "address": {
-//                       "company_name": "Example Corp.",
-//                       "address_line1": "4009 Marathon Blvd",
-//                       "city_locality": "Austin",
-//                       "state_province": "TX",
-//                       "postal_code": "78756",
-//                       "country_code": "US"
-//                       }
-//                   },
-//                   "ship_to": {
-//                       "account": "710535191",
-//                       "contact": {
-//                           "name": "John Doe",
-//                           "phone_number": "1111111111",
-//                           "email": "johndoe@test.com"
-//                       },
-//                       "address": {
-//                       "company_name": "Widget Company",
-//                       "address_line1": "525 S Winchester Blvd",
-//                       "city_locality": "San Jose",
-//                       "state_province": "CA",
-//                       "postal_code": "95128",
-//                       "country_code": "US"
-//                       }
-//                   },
-//                   "bill_to": {
-//                       "type": "third_party",
-//                       "payment_terms": "prepaid",
-//                       "account": "710535191",
-//                       "address": {
-//                       "company_name": "Example Corp.",
-//                       "address_line1": "4009 Marathon Blvd",
-//                       "city_locality": "Austin",
-//                       "state_province": "TX",
-//                       "postal_code": "78756",
-//                       "country_code": "US"
-//                       },
-//                       "contact": {
-//                           "name": "John Doe",
-//                           "phone_number": "1111111111",
-//                           "email": "johndoe@test.com"
-//                       }
-//                   },
-//                   "requested_by": {
-//                       "company_name": "Example Corp.",
-//                       "contact": {
-//                       "name": "John Doe",
-//                       "phone_number": "1111111111",
-//                       "email": "johndoe@test.com"
-//                       }
-//                   }
-//               },
-//               "shipment_measurements": {
-//                   "total_linear_length": {
-//                   "value": 48,
-//                   "unit": "inches"
-//                   },
-//                   "total_width": {
-//                   "value": 48,
-//                   "unit": "inches"
-//                   },
-//                   "total_height": {
-//                   "value": 48,
-//                   "unit": "inches"
-//                   },
-//                   "total_weight": {
-//                   "value": 10,
-//                   "unit": "pounds"
-//                   }
-//               }
-//          })
 //         var requestOptions = {
 //           method: 'POST',
 //           headers: myHeaders,
