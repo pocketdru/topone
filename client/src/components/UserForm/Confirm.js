0799a2aec0db8c17227407d17e72a1c6706fcf28
pocketdru <pocketdru@gmail.com>
2021-12-03 18:09:59 -0700

    step 3

--- client/src/components/UserForm/Confirm.js
+++ client/src/components/UserForm/Confirm.js
@@ -28,70 +28,48 @@ export class Confirm extends Component {
         var price = values.milePrice + values.carPrice;
 
         return (
-            <div>
-                <p>{values.name}</p>
-                <p>{values.year}</p>
-                <p>{values.model}</p>
-                <p>{values.make}</p>
-                <p>{values.trim}</p>
-                <p>{values.milePrice}</p> 
-                <p>{price}</p>
-                <div className="form-group">
-                <form className="contact-form needs-validation pb-4" onSubmit={this.sendEmail}>
-                    <div className="form-row">
-                    <div className="form-group col-md-4">     
-                        <label>Your Name</label>
-                        <input defaultValue={values.name} type="text" name="from_name" className="form-control" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your Email</label>
-                        <input defaultValue={values.email} type="email" name="from_email" className="form-control" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={values.phone} type="tel" name="phone" className="form-control" placeholder="optional" hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={values.puZip} type="tel" name="puZip" className="form-control" placeholder="optional" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={values.delZip} type="tel" name="delZip" className="form-control" placeholder="optional" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={values.year} type="tel" name="year" className="form-control" placeholder="optional" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={values.model} type="tel" name="model" className="form-control" placeholder="optional" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={values.make} type="tel" name="make" className="form-control" placeholder="optional" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={values.trim} type="tel" name="trim" className="form-control" placeholder="optional" required hidden/>
-                    </div>
-                    <div className="form-group col-md-4">
-                        <label>Your phone number</label>
-                        <input defaultValue={price} type="tel" name="price" className="form-control" placeholder="optional" required hidden/>
+            <section id="email">
+                <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                    <div className="form-group contact-form mt-3 mb-3 pt-3">
+                        <h3 className="card-title">Shipment details</h3>
+                        <p>{values.milePrice}</p> 
+                        <p>{price}</p>
+                        <ul class="list-group">
+                            <li class="card-text">{values.name}</li>
+                            <li class="card-text">{values.puZip}</li>
+                            <li class="card-text">{values.delZip}</li>
+                            <li class="card-text">{values.year}</li>
+                            <li class="card-text">{values.model}</li>
+                            <li class="card-text">{values.make}</li>
+                            <li class="card-text">{values.trim}</li>
+                        </ul>
+                        <form className="needs-validation pb-4" onSubmit={this.sendEmail}>
+                            <input defaultValue={values.name} type="text" name="from_name" className="form-control" required hidden/>
+                            <input defaultValue={values.email} type="email" name="from_email" className="form-control" required hidden/>
+                            <input defaultValue={values.phone} type="tel" name="phone" className="form-control" placeholder="optional" hidden/>
+                            <input defaultValue={values.puZip} type="tel" name="puZip" className="form-control" placeholder="optional" required hidden/>
+                            <input defaultValue={values.delZip} type="tel" name="delZip" className="form-control" placeholder="optional" required hidden/>
+                            <input defaultValue={values.year} type="tel" name="year" className="form-control" placeholder="optional" required hidden/>
+                            <input defaultValue={values.model} type="tel" name="model" className="form-control" placeholder="optional" required hidden/>
+                            <input defaultValue={values.make} type="tel" name="make" className="form-control" placeholder="optional" required hidden/>
+                            <input defaultValue={values.trim} type="tel" name="trim" className="form-control" placeholder="optional" required hidden/>
+                            <input defaultValue={price} type="tel" name="price" className="form-control" placeholder="optional" required hidden/>
+                        <div className="form-group d-flex justify-content-between">
+                            <button className="btn" onClick={this.back}>
+                                Back
+                            </button>
+                            <button className="btn" type="submit">
+                                Submit
+                            </button>
+                        </div>
+                        </form>
                     </div>
                     </div>
-                    <input type="submit" value="SEND MESSAGE" className="btn d-block mx-auto pl-4 pr-4 pt-3 pb-3" variant="primary" />
-                    </form>
-                        <button className="btn" type="submit">
-                            Continue
-                        </button>
                     </div>
-                    <div className="form-group">
-                        <button className="btn" onClick={this.back}>
-                            Back
-                        </button>
-                    </div>
-            </div>
+                </div>
+           </section>
         )
     }
 }
